d7f4867cb085163ec35ca96647a5e4fb5d15ecd6
navaneeth-s <53905307+navaneeth-s@users.noreply.github.com>
2020-08-25 21:43:05 +0530

    Create get_runner_data.py

---
+++ get_runner_data.py
@@ -0,0 +1,41 @@
+import sys
+import os
+current_dir = os.getcwd()
+first_only = True
+try:
+    genre_name = sys.argv[1]
+except:
+    genre_name = "social_entertainment"
+genre_dic = {}
+current_dir_name = current_dir.split("/")[-1]
+print(genre_name+"_bundle_dic" + " = {")
+for (dirpath, dirnames, filenames) in os.walk(genre_name):
+    if first_only:
+        dirs = dirnames
+        first_only = None
+    # print("dir path", dirpath)
+    # print("dir",dirnames)
+    # print("file", filenames)
+    dir_name = dirpath.split("/")[-1]
+    for filename in filenames:
+        if (filename.endswith(".py") and dir_name in dirs and (not dir_name.startswith("-"))):
+            path = dirpath.split("/headspin")[-1]
+            file_path = path+"/"+filename
+            # open_file_path = file_path.split(current_dir_name)[1]
+            # print(open_file_path)
+            # file_open = os.path(open_file_path)
+            path = os.path.join(dirpath)
+            path1 = path+"/" + filename
+            app_name_set = True
+            package_set = True
+            with open(path1, 'r') as f:
+                for line in f:
+                    line = line.rstrip()
+                    if "app_name" in line and "=" in line and "\"" in line and app_name_set:
+                        app_name = line.split("=")[-1]
+                        app_name_set = None
+                    if "package" in line and "=" in line and "\"" in line and package_set:
+                        package = line.split("=")[-1]
+                        package_set = None
+            print("\t"+package+" : "+"{\n\t\t"+"'path' : path_to_parent_folder + '"+file_path+"',\n\t\t'name' : "+app_name+"\n\t},")
+print("}")
